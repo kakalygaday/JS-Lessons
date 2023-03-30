2bf540e6fd3cab2e974128aab46c0f47d2367fcb
Omar Ojarov <83600309+O-Ojarov@users.noreply.github.com>
2023-03-30 14:40:58 +0300

    Closures exercise 7 is updated

---
+++ Closures7/exercise.js
@@ -0,0 +1,11 @@
+function printName(){
+    this.helloName = "Hello John";
+    function inner(){
+        setTimeout(() => {
+            console.log(this.helloName);
+        }, 1000)
+        return helloName;
+    }
+    return inner;
+}
+printName()();
