4fd1ebbd96f45fe424f1678fb479b9597bdedcf9
Omar Ojarov <83600309+O-Ojarov@users.noreply.github.com>
2023-03-30 14:28:14 +0300

    Closures exercise 6 is updated

--- Closures6/exercise.js
+++ Closures6/exercise.js
@@ -1,11 +1,20 @@
-function printName(){
-    let helloName = "Hello John";
-    function inner(){
-        // return helloName;
-    }
-    return helloName;
-}
-console.log(printName());
+// function printName(){
+//     let helloName = "Hello John";
+//     function inner(){
+//         // return helloName;
+//     }
+//     return helloName;
+// }
+// console.log(printName());
+
 // x = printName()
 // console.log(x);
 
+function printName(){
+    let helloName = "Hello John";
+    function inner(){
+        return helloName;
+    }
+    return inner;
+}
+console.log(printName()());
