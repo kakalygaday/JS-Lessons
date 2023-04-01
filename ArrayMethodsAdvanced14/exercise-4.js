89ef1ba3c9aa9889f092584e83261d79d4b9ef4f
Omar Ojarov <83600309+O-Ojarov@users.noreply.github.com>
2023-04-01 22:05:54 +0300

    Array Methods Advanced exercise 14 is done

---
+++ ArrayMethodsAdvanced14/exercise-4.js
@@ -0,0 +1,61 @@
+function uncompletedNotes(notes) {
+  let uncompletedNotesArr = [];
+  let x = notes.forEach(notesValue => notesValue.todos.filter(todosValue => {
+    if(!todosValue.done){
+      uncompletedNotesArr.push(todosValue);
+    }
+  }));
+  return uncompletedNotesArr;
+};
+
+const notes = [
+  {
+    id: 1,
+    description: "Workout program",
+    todos: [
+      {
+        id: 1,
+        name: "Push ups - 10 x 3",
+        done: false,
+      },
+      {
+        id: 2,
+        name: "Abdominals - 20 x 3",
+        done: true,
+      },
+      {
+        id: 3,
+        name: "Tapis Roulant - 15min",
+        done: true,
+      },
+    ],
+  },
+  {
+    id: 2,
+    description: "Front-end Roadmap",
+    todos: [
+      {
+        id: 1,
+        name: "Learn HTML",
+        done: true,
+      },
+      {
+        id: 2,
+        name: "Learn CSS",
+        done: true,
+      },
+      {
+        id: 3,
+        name: "Learn JavaScript",
+        done: true,
+      },
+      {
+        id: 4,
+        name: "Learn Angular",
+        done: false,
+      },
+    ],
+  },
+];
+
+console.log(uncompletedNotes(notes));
