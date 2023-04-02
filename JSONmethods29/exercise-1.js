2b60f704a585f16ce6980a74425f47e003133ebc
Omar Ojarov <83600309+O-Ojarov@users.noreply.github.com>
2023-04-02 16:48:04 +0300

    JSON Methods exercise 29 is done

---
+++ JSONmethods29/exercise-1.js
@@ -0,0 +1,14 @@
+class Person {
+  constructor(id, firstName, lastName, age) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+}
+
+const developer = new Person(1, 'Mario', 'Rossi', 25);
+console.log(typeof(developer));
+// Print developer as json object
+console.log(JSON.stringify(developer));
+console.log(typeof(JSON.stringify(developer)));
