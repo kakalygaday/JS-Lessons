865b136b0d662dfd9f1d0eae0356faa3b5fc5029
Omar Ojarov <83600309+O-Ojarov@users.noreply.github.com>
2023-04-02 09:22:19 +0300

    Destructuring exercise 20 is done

---
+++ Destructuring/exercise-1.js
@@ -0,0 +1,23 @@
+// let num1 = 10;
+// let num2 = 20;
+// console.log('Before swap: ', num1, num2); // Before swap: 10 20
+
+// // Variable swapping
+// let temp = num2;
+// num2 = num1;
+// num1 = temp;
+
+// console.log('After swap: ', num1, num2); // After swap: 20 10
+
+let [num1, num2] = [10, 20];
+console.log('Before swap: ', num1, num2); // Before swap: 10 20
+[num1, num2] = [num2, num1];
+console.log('After swap: ', num1, num2); // After swap: 20 10
+
+//--------Another way------------
+const twoNumbers = [10, 20];
+let [firstNumber, secondNumber] = twoNumbers;
+const temp = firstNumber;
+firstNumber = secondNumber;
+secondNumber = temp;
+console.log('After swap: ', firstNumber, secondNumber); // After swap: 20 10
