6ffac70dbe5c1c479fa7edbeadc9fe03b70e0876
Omar Ojarov <83600309+O-Ojarov@users.noreply.github.com>
2023-04-11 19:35:51 +0300

    checkpoint file is uploaded

---
+++ homeworks/js-checkpoint/js-checkpoint.js
@@ -0,0 +1,48 @@
+// use addEventListener to handle form submit event
+const form = document.querySelector('form')
+form.addEventListener('submit', function () {
+    var name = document.getElementById('name').value;
+    var surname = document.getElementById('surname').value;
+    var email = document.getElementById('email').value;
+    var number = document.getElementById('number').value;
+    formValidation(name, surname, email, number);
+
+})
+formValidation(name, surname, email, number).then(function (result) {
+    if (result) {
+        alert("Form is valid!") 
+    } else {
+        alert("Form is invalid, try again!");
+    }
+}).catch(function (error) {
+    console.log(error);  
+}); 
+
+function formValidation(name, surname, email, number) {
+    if (name.length > 0 && surname.length > 0 && email.length > 0 && number > 13) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function addAttendee(name, surname, email, number) {
+    var attendee = document.getElementById('attendee')
+    attendee.innerHTML =
+     `<li class="list-group-item d-flex justify-content-between align-items-start">
+        <div class="ms-2 me-auto">
+            <div class="fw-bold">${attendee.name}, ${attendee.surnamename}, ${attendee.number}
+            </div>
+            ${attendee.email}
+        </div>
+   <span class="badge bg-primary rounded-pill">M</span>
+     </li>`
+}
+function addAttendee(name, surname, email, number) {
+    return new Promise(function (resolve, reject) {
+        if (formValidation(name, surname, email, number)) {
+            resolve('Attendee added!'); 
+        } else { 
+            reject('error found!'); 
+        } 
+    }); 
+}
